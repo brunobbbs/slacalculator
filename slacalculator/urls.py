89f2fedc8ef83bf46ef40eda4eb7321b988db2a9
Bruno Barbosa <bsbruno1@gmail.com>
2015-08-07 15:40:22 -0300

    index page moved to core app

--- slacalculator/urls.py
+++ slacalculator/urls.py
@@ -2,8 +2,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views
 
+from core.views import HomePageView
+
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'accounts/login/', views.login, name='auth_login'),
     url(r'accounts/logout/', views.logout, name='auth_logout'),
+    url(r'^$', HomePageView.as_view(), name="index"),
 ]
