9abe83689ae1e67b9c98528240b1b68d2af12499
Bruno Barbosa <bsbruno1@gmail.com>
2015-08-07 18:11:26 -0300

    added project's tables on django admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Queue, Sla, Status, Ticket, Break
+
+
+class QueueAdmin(admin.ModelAdmin):
+
+    list_display = ("title", )
+
+
+class SlaAdmin(admin.ModelAdmin):
+
+    list_display = ("service", "queue", "days_budget", "days_delivery", "value")
+    list_filter = ("queue", )
+
+
+class StatusAdmin(admin.ModelAdmin):
+
+    list_display = ("title", )
+
+
+class TicketAdmin(admin.ModelAdmin):
+
+    search_fields = ("identifier", "title")
+    list_display = ("identifier", "title", "start_date", "end_date", "sla", "status")
+    list_filter = ("status", )
+
+
+class BreakAdmin(admin.ModelAdmin):
+
+    list_display = ("ticket", "date", "reason")
+    list_filter = ("status", )
+
+
+admin.site.register(Queue, QueueAdmin)
+admin.site.register(Sla, SlaAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Break, BreakAdmin)
