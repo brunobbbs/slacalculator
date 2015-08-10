426aecd17938e7bbbb4ec2a81248eadbdae877f6
Bruno Barbosa <bsbruno1@gmail.com>
2015-08-10 15:21:04 -0300

    changestatusview implemented

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url
 
-from .views import TicketCreateView, TicketListView, TicketUpdateView
+from .views import (
+    TicketCreateView,
+    TicketListView,
+    TicketUpdateView,
+    ChangeStatusView
+)
 
 
 urlpatterns = [
     url(r'^add/$', TicketCreateView.as_view(), name="add"),
     url(r'^list/$', TicketListView.as_view(), name="list"),
     url(r'^update/(?P<pk>\d+)/$', TicketUpdateView.as_view(), name="update"),
+    url(r'^status/update/(?P<pk>\d+)/', ChangeStatusView.as_view(), name="status_update"),
 ]
