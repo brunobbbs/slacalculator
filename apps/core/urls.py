8638bdf233812d48ac9de6dbc5c586d242f6a259
Bruno Barbosa <bsbruno1@gmail.com>
2015-08-10 17:22:05 -0300

    added ticket detail view

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
 from .views import (
+    TicketDetailView,
     TicketCreateView,
     TicketListView,
     TicketUpdateView,
@@ -9,6 +10,7 @@ from .views import (
 
 
 urlpatterns = [
+    url(r'^view/(?P<pk>\d+)/$', TicketDetailView.as_view(), name="view"),
     url(r'^add/$', TicketCreateView.as_view(), name="add"),
     url(r'^list/$', TicketListView.as_view(), name="list"),
     url(r'^update/(?P<pk>\d+)/$', TicketUpdateView.as_view(), name="update"),
