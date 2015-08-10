e12ef584c437ff132080d2762445405f8d91185d
Bruno Barbosa <bsbruno1@gmail.com>
2015-08-09 23:53:20 -0300

    added 'add', 'list' and 'update' urls

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,1 +1,10 @@
-__author__ = 'bruno'
+from django.conf.urls import url
+
+from .views import TicketCreateView, TicketListView, TicketUpdateView
+
+
+urlpatterns = [
+    url(r'^add/$', TicketCreateView.as_view(), name="add"),
+    url(r'^list/$', TicketListView.as_view(), name="list"),
+    url(r'^update/(?P<pk>\d+)/$', TicketUpdateView.as_view(), name="update"),
+]
