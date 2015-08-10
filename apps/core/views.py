a798ab14cf277435c38b8e7799722a2307372b7d
Bruno Barbosa <bsbruno1@gmail.com>
2015-08-10 15:20:48 -0300

    ChangeStatusView has been implemented

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+from datetime import datetime
+
 from django.views.generic import (
     TemplateView,
     CreateView,
@@ -9,8 +11,8 @@ from django.views.generic import (
 
 from django.core.urlresolvers import reverse
 
-from core.forms import TicketForm
-from .models import Ticket, Status
+from .forms import TicketForm, StatusForm
+from .models import Ticket, Status, StatusChoice
 
 
 class HomePageView(TemplateView):
@@ -41,10 +43,53 @@ class TicketUpdateView(UpdateView):
         return reverse("tickets:list")
 
 
-class StatusCreateView(CreateView):
+class ChangeStatusView(CreateView):
 
     model = Status
-    form_class = None  # TODO: Add form_class
+    form_class = StatusForm
 
     def get_success_url(self):
         return reverse("tickets:list")
+
+    def get_ticket(self):
+        ticket = Ticket.objects.get(pk=self.request.GET.get("ticket"))
+        return ticket
+
+    def get_status(self, status):
+        result = dict()
+        for choice, value in StatusChoice:
+            result[choice] = value
+        return result[status]
+
+    def get_context_data(self, **kwargs):
+        kwargs = super(ChangeStatusView, self).get_context_data(**kwargs)
+        state = self.request.GET.get("status")
+        kwargs.update({
+            'ticket': self.get_ticket(),
+            'status': self.get_status(state)
+        })
+        return kwargs
+
+    def form_valid(self, form):
+        current_status = self.get_object()
+        current_status.end_date = datetime.now()
+        current_status.save()
+
+        state = self.request.GET.get("status")
+        ticket = self.get_ticket()
+        if state == "closed":
+            ticket.end_date = datetime.now()
+            ticket.save()
+        else:
+            ticket.end_date = None
+            ticket.save()
+        return super(ChangeStatusView, self).form_valid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super(ChangeStatusView, self).get_form_kwargs()
+        state = self.request.GET.get("status")
+        kwargs['initial'] = {
+            'ticket': self.get_ticket().pk,
+            'state': state
+        }
+        return kwargs
