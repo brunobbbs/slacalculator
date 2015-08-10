d43fc6429e3304b2640f8217e44ce6a333f06fb5
Bruno Barbosa <bsbruno1@gmail.com>
2015-08-09 23:55:06 -0300

    fix some urls and added tickets url

--- slacalculator/urls.py
+++ slacalculator/urls.py
@@ -7,7 +7,8 @@ from core.views import HomePageView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'accounts/login/', views.login, name='auth_login'),
-    url(r'accounts/logout/', views.logout, name='auth_logout'),
+    url(r'^accounts/login/', views.login, name='auth_login'),
+    url(r'^accounts/logout/', views.logout, name='auth_logout'),
+    url(r'^tickets/', include('core.urls', namespace='tickets')),
     url(r'^$', HomePageView.as_view(), name="index"),
 ]
